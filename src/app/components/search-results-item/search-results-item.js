9be65cb5ca5b4f985d99b9f97c16a332c5462e30
Tomislav Cundic <tcundic@gmail.com>
2019-11-06 14:47:37 +0100

    Added component for items on search results page

---
+++ src/app/components/search-results-item/search-results-item.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import './search-results-item.scss';
+
+export function SearchResultsItem() {
+    return (
+        <div className="results">
+            <div className="row">
+                <div className="col-sm-12">
+                    <div className="card shadow">
+                        <div className="card-body">
+                            <h5 className="card-title">Special title treatment</h5>
+                            <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            <a href="#" className="btn btn-primary">View profile</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-sm-12">
+                    <div className="card shadow">
+                        <div className="card-body">
+                            <h5 className="card-title">Special title treatment</h5>
+                            <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            <a href="#" className="btn btn-primary">View profile</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-sm-12">
+                    <div className="card shadow">
+                        <div className="card-body">
+                            <h5 className="card-title">Special title treatment</h5>
+                            <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            <a href="#" className="btn btn-primary">View profile</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-sm-12">
+                    <div className="card shadow">
+                        <div className="card-body">
+                            <h5 className="card-title">Special title treatment</h5>
+                            <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            <a href="#" className="btn btn-primary">View profile</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/app/components/search-users-results/search-users-results.js
+++ src/app/components/search-users-results/search-users-results.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './search-users-results.scss';
+import { SearchResultsItem } from '../search-results-item/search-results-item';
 
 export function SearchUsersResults() {
     return (
-        <div className="page-content container">
-            Users
+        <div className="page-content container search-results-container">
+            <SearchResultsItem />
         </div>
     )
 }
