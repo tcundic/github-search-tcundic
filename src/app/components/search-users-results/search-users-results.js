a9ae07e144e7bb9bcc06ed66e088c3dd28069294
Tomislav Cundic <tcundic@gmail.com>
2019-11-10 21:26:46 +0100

    * Previous not commited changes

--- src/app/components/search-users-results/search-users-results.js
+++ src/app/components/search-users-results/search-users-results.js
@@ -2,26 +2,24 @@ import React from 'react';
 import './search-users-results.scss';
 import { SearchResultsItem } from '../search-results-item/search-results-item';
 
-export class SearchUsersResults extends React.Component {
-    render() {
-        let items = [];
+export const SearchUsersResults = ({searchResults, onClick}) => {
+    let items = [];
 
-        if (this.props.searchResults) {
-            this.props.searchResults.map((user) => {
-                items.push(<SearchResultsItem key={user.id} user={user} onClick={(userId) => this.props.onClick(userId)} />);
-            });
+    if (searchResults) {
+        searchResults.map((user) => {
+            items.push(<SearchResultsItem key={user.id} user={user} onClick={(userId) => onClick(userId)} />);
+        });
 
-            if (this.props.searchResults.length == 0) {
-                items.push(<em className="no-results" key="0">No results</em>);
-            }
+        if (searchResults.length == 0) {
+            items.push(<em className="no-results" key="0">No results</em>);
         }
-
-        return (
-            <div className="page-content container search-results-container">
-                <div className="row">
-                    {items}
-                </div>
-            </div>
-        )
     }
+
+    return (
+        <div className="page-content container search-results-container">
+            <div className="row">
+                {items}
+            </div>
+        </div>
+    )
 }
